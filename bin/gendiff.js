9d9b492d364f3488e58cab677dabcb9348add906
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-08 00:12:58 +0700

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
-.helpOption('-h, --help', 'display help for command');
+.option('-f, --format <type>', 'output format')
+.helpOption('-h, --help', 'output usage information')
+.argument('<filepath1> <filepath2>');
 program.parse();
