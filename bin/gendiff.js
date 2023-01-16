29eda74835e2d60bf91774d3d40e6b67c6a1ef1e
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-16 22:43:49 +0700

    add .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
+import genDiff from '../src/index.js';
 import { Command } from 'commander';
-const program = new Command();
 
+const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
 .option('-f, --format <type>', 'output format')
 .helpOption('-h, --help', 'output usage information')
-.argument('<filepath1> <filepath2>');
+.argument('<filepath1>')
+.argument('<filepath2>')
+.action((filetype1, filetype2) => {
+    const result = genDiff(filetype1, filetype2);
+    console.log(result);
+  });
 program.parse();
