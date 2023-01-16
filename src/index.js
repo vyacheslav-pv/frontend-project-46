120e365664322c2c2b01bf19569cd82e1b725b5e
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-16 22:26:35 +0700

    feat: add a comparison output of two files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import path from 'path';
+import fs from 'fs';
+import _ from 'lodash';
+import parser from './parsers.js';
+import getDiff from './getDiff.js';
+
+const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', filepath), 'utf8');
+const getFormat = (filepath) => filepath.split('.')[1];
+
+const genDiff = (filepath1, filepath2) => {
+    const data1 = parser(getData(filepath1), getFormat(filepath1));
+    const data2 = parser(getData(filepath2), getFormat(filepath2));
+
+   return getDiff(data1, data2);
+}
+  export default genDiff;
