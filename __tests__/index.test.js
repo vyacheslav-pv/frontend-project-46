ca1781e34ea926df90e49c1963db9c5becc75e57
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-23 00:22:57 +0700

    Fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,11 +9,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const fileExtensions = ['json', 'yaml', 'yml'];
+const fileExtensions = ['json', 'yaml'];
 
 test.each(fileExtensions)('Comparing flat JSON files', (format) => {
-  const file1path = getFixturePath(`file1${format}`);
-  const file2path = getFixturePath(`file2${format}`);
+  const file1path = getFixturePath(`file1.${format}`);
+  const file2path = getFixturePath(`file2.${format}`);
   const result = readFile('resultDiff.txt');
   expect(genDiff(file1path, file2path)).toEqual(result);
 });
