e69dcc6c4304d0496ed596efea1dc207a3dcba51
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-29 20:28:52 +0700

    chore: add default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,12 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filetype1, filetype2) => {
-    const result = genDiff(filetype1, filetype2);
+    const result = genDiff(filetype1, filetype2, program.opts().format);
     console.log(result);
   });
 program.parse();
