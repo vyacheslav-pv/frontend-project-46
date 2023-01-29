6be32af20b4804f8ef6fd1d866bd0f66ce32c147
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-29 20:36:47 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import parser from './parsers.js';
 import getDiff from './getDiff.js';
+import formatStylish from './formatters/stylish.js';
 
 const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', filepath), 'utf8');
 const getFormat = (filepath) => filepath.split('.')[1];
@@ -10,6 +11,7 @@ const genDiff = (filepath1, filepath2) => {
   const data1 = parser(getData(filepath1), getFormat(filepath1));
   const data2 = parser(getData(filepath2), getFormat(filepath2));
 
-  return getDiff(data1, data2);
+  const result = getDiff(data1, data2);
+  return formatStylish(result);
 };
 export default genDiff;
