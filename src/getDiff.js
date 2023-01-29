763974bb5d0654855b3d70efe9722309bc448729
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-01-29 20:34:22 +0700

    feat: comparison files with nested structure

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,21 +1,28 @@
 import _ from 'lodash';
 
 const getDiff = (data1, data2) => {
-  const indent1 = ' '.repeat(4);
-  const indent2 = ' '.repeat(2);
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
-  const result = keys.flatMap((key) => {
-    if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
-      return [`${indent2}- ${key}: ${data1[key]}`, `${indent2}+ ${key}: ${data2[key]}`];
+
+  const result = keys.map((key) => {
+    if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+      return { type: 'nested', name: key, children: getDiff(data1[key], data2[key]) };
     }
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return `${indent2}- ${key}: ${data1[key]}`;
+    if (!_.has(data1, key)) {
+      return { type: 'added', name: key, value: data2[key] };
     }
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      return `${indent2}+ ${key}: ${data2[key]}`;
+    if (!_.has(data2, key)) {
+      return { type: 'deleted', name: key, value: data1[key] };
     }
-    return `${indent1}${key}: ${data1[key]}`;
+    if (data1[key] !== data2[key]) {
+      return {
+        type: 'changed',
+        name: key,
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
+    return { type: 'unchanged', name: key, value: data1[key] };
   });
-  return ['{', ...result, '}'].join('\n');
+  return result;
 };
 export default getDiff;
