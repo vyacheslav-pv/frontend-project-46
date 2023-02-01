721ed9ac2922a7b573bb93d54853ed59d2f163cc
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-02-01 23:50:45 +0700

    feat: add format selection

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import path from 'path';
 import fs from 'fs';
 import parser from './parsers.js';
 import getDiff from './getDiff.js';
-import formatStylish from './formatters/stylish.js';
+import format from './formatters/index.js';
 
 const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', filepath), 'utf8');
 const getFormat = (filepath) => filepath.split('.')[1];
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parser(getData(filepath1), getFormat(filepath1));
   const data2 = parser(getData(filepath2), getFormat(filepath2));
 
   const result = getDiff(data1, data2);
-  return formatStylish(result);
+  return format(result, formatName);
 };
 export default genDiff;
